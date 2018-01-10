c45969e80a29cf835c67fd4523073303549fef0a
Scottbromander <scottbromander@gmail.com>
2018-01-10 11:30:19 -0700

    More comments

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        //Print a welcome
         System.out.println("Enter your name:");
 
         //Dis is my robot!
