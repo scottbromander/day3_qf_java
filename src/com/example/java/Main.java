45c65f164aee830d53a53cec31689603ec2cd688
Scottbromander <scottbromander@gmail.com>
2018-01-10 11:25:54 -0700

    Updated with notes

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
+    //This application asks you your name, and then it says Hi!
     public static void main(String[] args) {
 	// write your code here
 
